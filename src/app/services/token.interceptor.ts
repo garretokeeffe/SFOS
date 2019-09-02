ec0b53f2df88a890f76cc08904f5aa49babddd98
DavidMcDowell <david.mcdowell@agriculture.gov.ie>
2019-09-02 10:23:33 +0100

    fixed up typescript syntax in token.interceptor.ts

--- src/app/services/token.interceptor.ts
+++ src/app/services/token.interceptor.ts
@@ -1,29 +1,29 @@
-import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
+import { Observable } from 'rxjs';
+
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-    constructor(protected keycloakService: KeycloakService) {}
-    tokenData: String;
-     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
- 
-   
-   const tokenPromise: Promise<String> = this.keycloakService.getToken();
-            tokenPromise.then(token => {
-                    this.tokenData=token
-                      console.log('token data :  '+this.tokenData);
-                });
+  private tokenData: string;
+
+  constructor(protected keycloakService: KeycloakService) {}
+
+  public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<string>> {
+    this.keycloakService.getToken().then((token: string) => {
+      this.tokenData = token;
+      // console.log('token data :  ' + this.tokenData);
+    });
     request = request.clone({
        setHeaders: {
-        Authorization: `Bearer ${this.tokenData}`
-      }
+        Authorization: `Bearer ${this.tokenData}`,
+      },
     });
     return next.handle(request);
   }
